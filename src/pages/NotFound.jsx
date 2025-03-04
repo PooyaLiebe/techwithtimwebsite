845f560be2ee6f427d65d6a029bbea9eab930a55
PooyaLiebe <pooya.payvar26@gmail.com>
2025-03-04 18:37:07 +0330

    Add files via upload

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 NOT FOUND</h1>
+      <p>The Page You Are Looking Doesn't Exist !</p>
+    </div>
+  );
+}
+
+export default NotFound;
